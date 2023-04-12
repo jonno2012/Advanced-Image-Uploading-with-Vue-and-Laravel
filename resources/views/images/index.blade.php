f270ad9d458e1cb6e53b57aa9721d3d66e6981af
jonno2012 <jonathanrhill78@gmail.com>
2023-04-12 18:01:20 +0100

    Adding vue to laravel using L10 and vite and vue3

--- resources/views/images/index.blade.php
+++ resources/views/images/index.blade.php
@@ -11,6 +11,9 @@
 <body>
     <div class="max-w-lg mx-auto mt-24">
         <h1 class="text-4xl font-bold text-center">Image Uploader</h1>
+        <div id="app"></div>
     </div>
+
+    @vite('resources/js/app.js')
 </body>
 </html>
